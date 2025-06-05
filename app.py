85d4dd3c9b4c43ec3dc2ac0b07a19d1aa87ad80e
Amir Hassan Shariatmadari <amirhassanshariatmadari@gmail.com>
2025-06-05 16:17:20 -0400

    add pylint workflow

--- app.py
+++ app.py
@@ -11,19 +11,19 @@ css_file = current_dir / "styles" / "main.css"
 resume_file = current_dir / "assets" / "amir_resume.pdf"
 profile_pic = current_dir / "assets" / "prof_pic.jpeg"
 
-
 # --- GENERAL SETTINGS ---
 PAGE_TITLE = "Digital Resume | Amir Hassan Shariatmadari"
 PAGE_ICON = ":wave:"
 NAME = "Amir Hassan Shariatmadari"
-DESCRIPTION = """
-Machine Learning and Software Engineer at IBSS Corp. 
-"""
+DESCRIPTION = """Machine Learning and Software Engineer at IBSS Corp."""
+
 EMAIL = "amirhassanshariatmadari@gmail.com"
+
 SOCIAL_MEDIA = {
     "LinkedIn": "https://linkedin.com",
     "GitHub": "https://github.com"
     }
+
 PROJECTS = {
     "🏆 Sales Dashboard - Comparing sales across three stores": "https://youtu.be/Sb0A9i6d320",
     "🏆 Income and Expense Tracker - Web app with NoSQL database": "https://youtu.be/3egaMfE9388",
