a002e92b2710a334292b3efcb835842cf2340f49
Amir Hassan Shariatmadari <amirhassanshariatmadari@gmail.com>
2025-06-05 20:57:35 -0400

    add css styling

--- app.py
+++ app.py
@@ -59,3 +59,9 @@ with col2:
     )
     st.write("📫", EMAIL)
 
+
+# --- SOCIAL LINKS ---
+st.write('\n')
+cols = st.columns(len(SOCIAL_MEDIA))
+for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
+    cols[index].write(f"[{platform}]({link})")
